3b2ec9b05cf8ea5b3a0b447931eacd84fd7b8257
forvisualstudio40@hotmail.com <forvisualstudio40@hotmail.com>
2024-09-22 22:29:08 +0300

    22/09/24

---
+++ PortfolioProjectNight/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        } 
+        
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavBar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ PortfolioProjectNight/Controllers/SkillController.cs
@@ -0,0 +1,56 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult SkillList()
+        {
+            var value = context.Skills.ToList();
+            return View(value);
+        }
+
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)
+        {
+            context.Skills.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skills.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateSkill(Skill skill)
+        {
+            var value = context.Skills.Find(skill.SkillId);
+            value.SkillName = skill.SkillName;
+            value.Rate = skill.Rate;
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = context.Skills.Find(id);
+            context.Skills.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+    }
+}
