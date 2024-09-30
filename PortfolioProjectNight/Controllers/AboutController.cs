379f1c5850212e7f8562e0252f0d47e6564c63e0
forvisualstudio40@hotmail.com <forvisualstudio40@hotmail.com>
2024-09-30 21:46:30 +0300

    09/30/2024

---
+++ PortfolioProjectNight/Controllers/AboutController.cs
@@ -0,0 +1,59 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class AboutController : Controller
+    {
+        DbMyPortfolioNightEntities db = new DbMyPortfolioNightEntities();
+        public ActionResult AboutList()
+        {
+            var values = db.Abouts.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAbout(About about)
+        {
+            db.Abouts.Add(about);
+            db.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+
+        public ActionResult DeleteAbout(int id)
+        {
+            var value = db.Abouts.Find(id);
+            db.Abouts.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateAbout(int id)
+        {
+            var value = db.Abouts.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAbout(About about)
+        {
+            var value = db.Abouts.Find(about.AboutId);
+
+            value.Title = about.Title;
+            value.Description = about.Description;
+            db.SaveChanges();
+            return RedirectToAction("AboutList");
+        }
+    }
+}
